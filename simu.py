80930cc0ac9abdc81faffdba1991110568c752c2
AMITKESARI2000 <kesari.amit33@gmail.com>
2021-02-23 21:03:51 +0530

    add data segments and find comments and .data

--- simu.py
+++ simu.py
@@ -1,1 +1,46 @@
 import re
+
+file = open("testingswap.s", "r")
+lines = file.readlines()
+file.close()
+
+# Global Storages
+RAM = [];
+PC = 0
+
+r = [0]
+at = 0
+v = [0] * 2
+a = [0] * 4
+t = [0] * 10
+s = [0] * 9
+k = [0] * 2
+p = 0x10008000
+sp = 0x7ffff8bc
+ra = 0
+
+is_program_done = False
+i = 0
+
+while i < len(lines):
+    lines[i] = lines[i].strip().lower()
+    # print(i, line)
+
+    # COMMENTS
+    if re.findall(r"# *", lines[i]):
+        print("it is a comment")  # comment can be added in middle also
+        i += 1
+        continue
+
+    # .data
+    if re.search(r"^\.data", lines[i]):
+        print("data", lines[i])
+
+        while True:
+            lines[i] = lines[i].strip().lower()
+            i += 1
+            print(lines[i])
+            if re.findall(r"^\.text", lines[i]):
+                i -= 1;
+                break
+    i += 1
